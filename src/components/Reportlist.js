3621809337d8821d527e0def286554be322aac7d
mkeino <mikko.keino@gmail.com>
2018-08-29 17:07:51 +0300

    pieni muutos

--- src/components/Reportlist.js
+++ src/components/Reportlist.js
@@ -16,13 +16,7 @@ class Reportlist extends Component{
             search:''
         }
     }
-    filterReports = () => {
-            console.log(this.state.markers[0].date)
-            console.log(new Date().toDateString())
-            // var a=this.state.markers;                
-            // var b = a.sort(function(a, b){return a.date>b.date});
-            // console.log(b);
-            
+    filterReports = () => {           
             if(this.state.selected===2 || this.state.selected===null){
                 let tempToilets = this.state.markers.sort((a, b) => (a.date > b.date ? 1 : -1))
                 this.setState({ markers: tempToilets })
@@ -32,13 +26,7 @@ class Reportlist extends Component{
                 let tempToilets = this.state.markers.sort((a, b) => (a.date < b.date ? 1 : -1))
                 this.setState({ markers: tempToilets })
                 this.setState({selected:2})
-            }
-            
-            
-            
-            
-            // let tempToilets = this.state.markers.filter(x => x.date <new Date().toDateString())
-            // this.setState({ markers: b })
+            }            
     }
 
     componentDidMount() {
